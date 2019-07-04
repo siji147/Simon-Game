2ab8a38ab350ffb4620c63c369bcaad154423c92
sijibomi <sijiajayi1@gmail.com>
2019-07-04 14:58:30 +0000

    Added the random color function

--- app.js
+++ app.js
@@ -6,22 +6,43 @@ function pressButton(color) {
         $(this).addClass("pressed").delay(50).queue(function () {
             $(this).removeClass("pressed").dequeue();
         })
-        // $(`#${color}`).fadeOut(50, function () {
-        //     $(`#${color}`).addClass("pressed")
-        // }).fadeIn(50, function () {
-        //     $(`#${color}`).removeClass("pressed")
-        // });
 
     });
 }
-
-// .addClass("error").delay(1000).queue(function(){
-//     $(this).removeClass("error").dequeue();
-
 
 pressButton('green');
 pressButton('red');
 pressButton('yellow');
 pressButton('blue');
 
-// .fadeOut(100).fadeIn(100)
+
+
+function blinkRandomColor() {
+
+    let color = {
+        'green': 1,
+        'red': 2,
+        'yellow': 3,
+        'blue': 4
+    }
+
+    var randomColor = (Math.floor(Math.random() * 4)) + 1;
+
+    return Object.keys(color)[randomColor - 1]
+}
+
+
+function gameOver() {
+    // $("body").css({
+    //     "background-color": "red",
+    //     "opacity": 0.8
+    // }).delay(50).queue(function () {
+    //     $(this).css({
+    //         "background-color": "#011F3F",
+    //         "opacity": 0
+    //     }).dequeue();
+    // })
+
+}
+
+$("body").on("click", gameOver);
--- testwork.js
+++ testwork.js
@@ -1,2 +1,18 @@
-var rand = (Math.floor(Math.random() * 4)) + 1;
-console.log(rand);
+// var rand = (Math.floor(Math.random() * 4)) + 1;
+// console.log(rand);
+
+function blinkRandomColor() {
+
+    let color = {
+        'green': 0,
+        'red': 1,
+        'yellow': 2,
+        'blue': 3
+    }
+
+    var randomColor = (Math.floor(Math.random() * 4)) + 1;
+
+    return Object.keys(color)[randomColor - 1];
+}
+
+console.log(blinkRandomColor());
